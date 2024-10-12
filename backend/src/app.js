e5b8d2ae7d5eee4071bd0ef05ad9138b81075906
Krypton321123 <coolrajsharma28@gmail.com>
2024-10-12 15:50:19 +0530

    Wrote a authentication middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,13 @@
 import express from 'express'; 
 import cors from 'cors'
 import { userRouter } from './routes/userRoutes.js';
+import cookieParser from 'cookie-parser'
+
 const app = express(); 
 
 app.use(express.json()); 
 app.use(cors()); 
+app.use(cookieParser()); 
 
 // app.get("/", (req, res) => {
 //     res.status(200).json({
