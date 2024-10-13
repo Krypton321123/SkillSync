827473a18063354dad60a728fdde564732c602bf
Krypton321123 <coolrajsharma28@gmail.com>
2024-10-13 13:45:47 +0530

    Added join controller

--- backend/src/controllers/communityController.js
+++ backend/src/controllers/communityController.js
@@ -3,6 +3,7 @@ import { ApiError } from "../utils/ApiError";
 import { ApiResponse } from "../utils/ApiResponse";
 import { isEmpty } from "../utils/isEmptyFields";
 import { Community } from "../models/community.model";
+import mongoose from "mongoose";
 
 const createCommunityController = asyncHandler(async (req, res) => {
     const user = req.user; 
@@ -80,6 +81,43 @@ const joinCommunity = asyncHandler(async (req, res) => {
 
         return res.status(200).json(new ApiResponse(200, dataToSend, "User joined Successfully"))
     } catch(err) {
+        return res.status(500).json(new ApiError(500, "Internal Server Error"))
+    }
+})
+
+const leaveCommunity = asyncHandler(async (req, res) => {
+    const user = req.user; 
+
+    try {
+        const { communityID } = req.params; 
+
+        if(!mongoose.Types.ObjectId.isValid(communityID)) {
+            return res.status(400).json(new ApiError(400, "Invalid community ID")); 
+        }
+
+        const userAlreadyJoined = await Community.findOne({
+            _id: communityID, 
+            members: { $in: [user._id]} 
+        })
+
+        if(!userAlreadyJoined) {
+            return res.status(404).json(new ApiError(404, "user not a member")); 
+        }
+
+        const updatedCommunity = await Community,findOneAndUpdate(
+            {_id: communityID}, 
+            {$pull: { members: user._id }}, 
+            {new: true}
+        ); 
+
+        if(!updatedCommunity) {
+            return res.status(400).json(new ApiError(400, "Community not updated")); 
+        }
+
+        return res.status(200).json(new ApiResponse(200, {id: communityID, members: updatedCommunity.members, membersCount: updatedCommunity.members.length}, "User left successfully")); 
+
+    } catch(err) {
+        console.log(err)
         return res.status(500).json(new ApiError(500, "Internal Server Error"))
     }
 })
