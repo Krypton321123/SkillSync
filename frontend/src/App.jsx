ac4f8498bc065cdb657e36e1765d3dceedd227a1
mukti <muktimishra0495@gmail.com>
2024-10-13 10:56:28 +0530

    loginController update , cloudinary setup and  hook up of signup and login with backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import SignUp from './components/SignUp.jsx'
+import Login from './components/Login.jsx'
 
 
 function App() {
 
   return (
     <>
-      <SignUp />
+      {/* <SignUp /> */}
+      <Login />
     </>
   )
 }
--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -1,48 +1,74 @@
-import React from 'react'
+import React, { useState } from 'react';
 import {
   FormControl,
   FormLabel,
   Input,  
   Flex,
   Center,  
-} from '@chakra-ui/react'
-import { Button } from '@chakra-ui/react'
+} from '@chakra-ui/react';
+import { Button } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 function SignupCard() {
+  const [formData , setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    emailId: '',
+    password: '',
+    confirmPassword: ''
+  })
+
+  const handleChange = (e)=> {
+    setFormData({...formData , [e.target.name]:e.target.value});
+  }
+
+  const handleSubmit = async(e)=> {
+    e.preventDefault();  // Prevent the default form submission behavior
+
+    try {
+      const response = await axios.post("http://localhost:5173/api/v1/use/signup", formData);
+      console.log(response);
+    } catch(error) {
+      console.log("Error in signing up", error);
+    }
+  }
+
   return (
     <Center height="100vh"> 
-      <Flex flexDirection="column" height={420} width={400} p={6} boxShadow="lg" borderRadius="md" bg="7E60BF">
+      <Flex as="form" onSubmit={handleSubmit} flexDirection="column" height={435} width={400} p={6} boxShadow="lg" borderRadius="md" bg="7E60BF">
         <FormLabel fontSize="2xl" mb={6}>Create an account</FormLabel>
+        
         <Flex flexDirection="row" gap={3}>
           <FormControl isRequired>
             <FormLabel>First name</FormLabel>
-            <Input placeholder='First name' />
+            <Input placeholder='First name' name='firstName' value={formData.firstName} onChange={handleChange}/>
           </FormControl>
           <FormControl isRequired>
             <FormLabel>Last name</FormLabel>
-            <Input placeholder='Last name' />
+            <Input placeholder='Last name' name='lastName' value={formData.lastName} onChange={handleChange}/>
           </FormControl>
         </Flex>
 
         <FormControl isRequired mt={4}>
           <FormLabel>Email</FormLabel>
-          <Input type="email" placeholder='Email' />
+          <Input type="email" placeholder='Email' name='emailId' value={formData.emailId} onChange={handleChange}/>
         </FormControl>
         <FormControl isRequired mt={4}>
           <FormLabel>Password</FormLabel>
-          <Input type="password" placeholder='Password' />
+          <Input type="password" placeholder='Password' name='password' value={formData.password} onChange={handleChange}/>
         </FormControl>
         <FormControl isRequired mt={4}>
           <FormLabel>Confirm Password</FormLabel>
-          <Input type="password" placeholder='Confirm Password' />
+          <Input type="password" placeholder='Confirm Password' name='confirmPassword' value={formData.confirmPassword} onChange={handleChange}/>
         </FormControl>
         
         <Center>
-          <Button colorScheme='purple' mt={6}>Create Account</Button>
+          <Button type="submit" colorScheme='purple' mt={6}>Create Account</Button>
         </Center>
       </Flex>
     </Center>
-  )
+  );
 }
 
 export default SignupCard;
