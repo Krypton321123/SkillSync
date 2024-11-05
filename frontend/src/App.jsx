ef57098961a24f826ce060673d059d89ee216d9c
mukti <muktimishra0495@gmail.com>
2024-11-05 21:35:23 +0530

    Post Card Update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,30 @@
 import { useState } from 'react'
-import SignUp from './components/SignUp.jsx'
-import Login from './components/Login.jsx'
-import CreateCommunityCard from './components/CreateCommunityCard.jsx'
+import SignUp from './components/AuthenticationComponents/SignUp.jsx'
+import Login from './components/AuthenticationComponents/Login.jsx'
+import CreateCommunityCard from './components/CommunityComponents/CreateCommunityCard.jsx'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import AppBar from './components/global/appBar.jsx'
 import AppBarForAuth from './components/global/AppbarForAuth.jsx'
 // import AppBar from './components/global/appBar.jsx'
+import PostCard from './components/PostComponents/PostCard.jsx'
 
 
 function App() {
 
   return (
+    // <>
+    //   <BrowserRouter>
+    //     <Routes>
+    //       <Route path="/signup" element={<AppBarForAuth><SignUp /></AppBarForAuth>}/>
+    //       <Route path="/signin" element={<AppBarForAuth><Login /></AppBarForAuth>}/>
+    //       <Route path="/test" element={<AppBarForAuth><Login /></AppBarForAuth>}/> 
+    //       {/* // this route is for testing */}
+    //     </Routes>
+    //   </BrowserRouter>
+    // </>
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/signup" element={<AppBarForAuth><SignUp /></AppBarForAuth>}/>
-          <Route path="/signin" element={<AppBarForAuth><Login /></AppBarForAuth>}/>
-          {/*<Route path="/test" element={<AppBarForAuth><Login /></AppBarForAuth>}/>*/} // this route is for testing
-        </Routes>
-      </BrowserRouter>
+    <PostCard />
+    
     </>
   )
 }
